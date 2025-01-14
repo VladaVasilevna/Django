aca1cf9a41b4cb65cdccba9aaf6b063bc250541a
vlada vasilevna <vladusik-home@mail.ru>
2025-01-14 17:13:07 +0300

    admin done

--- catalog/models.py
+++ catalog/models.py
@@ -1,6 +1,19 @@
 from django.db import models
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Категория продукта", help_text="Введите название категории")
+    description = models.TextField(
+        blank=True, null=True, verbose_name="Описание", help_text="Введите описание категории"
+    )
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
+
+    def __str__(self):
+        return self.name
+
 class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name="Продукт", help_text="Введите название продукта")
     description = models.TextField(
@@ -13,7 +26,15 @@ class Product(models.Model):
         verbose_name="Изображение",
         help_text="Загрузите изображение продукта",
     )
-    category = models.CharField(max_length=100, verbose_name="Категория", help_text="Введите категорию продукта")
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        verbose_name="Категория",
+        help_text="Введите категорию продукта",
+        null=True,
+        blank=True,
+        related_name="products",
+    )
     price = models.IntegerField(verbose_name="Цена за покупку", help_text="Введите цену продукта")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата последнего изменения")
@@ -22,20 +43,6 @@ class Product(models.Model):
         verbose_name = "Продукт"
         verbose_name_plural = "Продукты"
         ordering = ["name", "category", "price"]
-
-    def __str__(self):
-        return self.name
-
-
-class Category(models.Model):
-    name = models.CharField(max_length=100, verbose_name="Категория продукта", help_text="Введите название категории")
-    description = models.TextField(
-        blank=True, null=True, verbose_name="Описание", help_text="Введите описание категории"
-    )
-
-    class Meta:
-        verbose_name = "Категория"
-        verbose_name_plural = "Категории"
 
     def __str__(self):
         return self.name
