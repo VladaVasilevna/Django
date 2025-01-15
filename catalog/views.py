367e7475414a1bbdd095456d2a8ac8e401d06285
vlada vasilevna <vladusik-home@mail.ru>
2025-01-15 18:17:04 +0300

    fixture done

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 
 def home(request):
-    return render(request, 'home.html')
+    # Получаем последние 5 созданных продуктов
+    latest_products = Product.objects.order_by('-created_at')[:5]
+
+    # Выводим их в консоль для отладки
+    print(latest_products)
+
+    # Передаем последние продукты в контекст шаблона
+    return render(request, 'home.html', {'latest_products': latest_products})
 
 
 def contacts(request):
