130d62500c947f0bf8ac9e80d2baead2ee81e9e1
vlada vasilevna <vladusik-home@mail.ru>
2025-01-27 18:04:43 +0300

    done 4

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from .views import (
+    HomePageView,
     BlogPostListView,
     BlogPostDetailView,
     BlogPostCreateView,
@@ -10,9 +13,10 @@ from .views import (
 app_name = 'blog'
 
 urlpatterns = [
-    path('', BlogPostListView.as_view(), name='post_list'),
+    path('', HomePageView.as_view(), name='base'),
+    path('all_posts/', BlogPostListView.as_view(), name='all_posts'),
     path('post/<int:pk>/', BlogPostDetailView.as_view(), name='post_detail'),
     path('post/new/', BlogPostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>/edit/', BlogPostUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/delete/', BlogPostDeleteView.as_view(), name='post_delete'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -8,6 +8,10 @@ class BlogPostListView(ListView):
     model = BlogPost
     template_name = 'blog/blogpost_list.html'
     context_object_name = 'posts'
+    paginate_by = 10
+
+    def get_queryset(self):
+        return BlogPost.objects.filter(is_published=True).order_by('-created_at')
 
 
 class BlogPostDetailView(DetailView):
@@ -34,3 +38,12 @@ class BlogPostDeleteView(DeleteView):
     model = BlogPost
     template_name = 'blog/blogpost_confirm_delete.html'
     success_url = reverse_lazy('blog:post_list')
+
+
+class HomePageView(BlogPostListView):
+    template_name = 'blog/base.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['latest_posts'] = BlogPost.objects.all().order_by('-created_at')[:2]
+        return context
