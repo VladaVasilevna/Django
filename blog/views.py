8ff0e245ff70ec2a315eb53289c5d3324008d7b1
vlada vasilevna <vladusik-home@mail.ru>
2025-01-29 13:24:05 +0300

    done

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,18 @@
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.views import View
 from .models import BlogPost
 from .forms import BlogPostForm
 
 
-class BlogPostListView(ListView):
+class BlogBaseView(View):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_blog'] = True
+        return context
+
+
+class BlogPostListView(BlogBaseView, ListView):
     model = BlogPost
     template_name = 'blog/blogpost_list.html'
     context_object_name = 'posts'
@@ -14,27 +22,27 @@ class BlogPostListView(ListView):
         return BlogPost.objects.filter(is_published=True).order_by('-created_at')
 
 
-class BlogPostDetailView(DetailView):
+class BlogPostDetailView(BlogBaseView, DetailView):
     model = BlogPost
     template_name = 'blog/blogpost_detail.html'
     context_object_name = 'post'
 
 
-class BlogPostCreateView(CreateView):
+class BlogPostCreateView(BlogBaseView, CreateView):
     model = BlogPost
     form_class = BlogPostForm
     template_name = 'blog/blogpost_form.html'
     success_url = reverse_lazy('blog:post_list')
 
 
-class BlogPostUpdateView(UpdateView):
+class BlogPostUpdateView(BlogBaseView, UpdateView):
     model = BlogPost
     form_class = BlogPostForm
     template_name = 'blog/blogpost_form.html'
     success_url = reverse_lazy('blog:post_list')
 
 
-class BlogPostDeleteView(DeleteView):
+class BlogPostDeleteView(BlogBaseView, DeleteView):
     model = BlogPost
     template_name = 'blog/blogpost_confirm_delete.html'
     success_url = reverse_lazy('blog:post_list')
@@ -46,4 +54,5 @@ class HomePageView(BlogPostListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['latest_posts'] = BlogPost.objects.all().order_by('-created_at')[:2]
+        context['is_base'] = True
         return context
